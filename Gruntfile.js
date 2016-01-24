91c16f15cbdcc63b8331232bcb1446a2110bb203
Arun Michael Dsouza <amdsouza92@gmail.com>
2016-01-24 22:48:44 +0530

    Initial build - All sections added with scrollspy

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        cssmin: {
+            target: {
+                files: {
+                    'css/theme.min.css': ['css/theme.css']
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: ['css/theme.css'],
+                tasks: ['cssmin'],
+                options: {
+                    spawn: false,
+                },
+            },
+        },
+        concat: {
+            libs: {
+                src: ['libs/jquery.min.js', 'libs/jquery-migrate.min.js', 'libs/mediaelement-and-player.min.js'],
+                dest: 'libs/build/vendor.min.js',
+            },
+            js: {
+                src: ['js/scrollspy.js', 'js/custom.js'],
+                dest: 'js/build/app.js',
+            }
+        },
+        uglify: {
+            my_target: {
+                files: {
+                    'js/build/app.min.js': ['js/build/app.js']
+                }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('default', ['watch']);
+};
